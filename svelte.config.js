c83bfbb0f871c2f80138c624d72a19818d5565ee
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-06-11 20:28:41 +0300

    add default stylesheets

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocessor from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: preprocessor({ scss: { prependData: `@use '../styles/config/_index.scss'as *;` } }),
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
